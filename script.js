a850a1ed44c34a687cfbfd727bf5544cdeef713b
Shantanu Kumar Mahanta <121172938+shantanukumar-dev@users.noreply.github.com>
2024-06-24 17:05:21 +0530

    Add files via upload
    This is the second part of the Javascript lecture.In this session  we disscussed about the variable.We can create variable in javascript by using three keyword i.e 1.let 2.var 3.const
    and also disscuss What is the difference among these three variable.

---
+++ script.js
@@ -0,0 +1,42 @@
+// We can declare a variable in java script  by using the three  keyword
+//                    1.let
+//                    2.var
+//                    3.const
+let a = 5;
+console.log(a);
+
+var b = "hello";
+console.log(b);
+
+let myname = "shantanu kumar";
+console.log(myname);
+
+
+//Const is used for declaring a constant variable ,The value of the respective variable canot be change during the program.
+
+const $smita = "Nibedita";
+console.log($smita);
+
+let _s1 = "shakti";
+console.log(_s1);
+
+// What is difference between let var and const?
+
+// By using Var keyword We can create Two variable  having same name.
+// it does not provide reliable so that must of the time we have to use let and const
+
+var name = "Keanu veers";
+var name = "jhon cina";
+console.log(name);
+
+ //The following code is totally error . Also let is  block-scoped variable
+// let num = 56;
+// let num = 57;
+// console.log(num);
+
+//It is totally wrong Once we create ,The value canot be changed.
+// const fname="shantanu";
+// fname="shantanu kumar";
+// console.log(fname);
+
+
